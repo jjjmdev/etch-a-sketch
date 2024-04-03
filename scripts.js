9ba0e48cc1d8a7950f507071bf41a7d48eac6547
Joshua Mamawag <162310653+jjjmdev@users.noreply.github.com>
2024-04-03 22:02:48 +0800

    Generate rows and items

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const gridContainer = document.querySelector("#grid-container");
+
+let n = 16;
+
+for (let j = 1; j <= n; j++) {
+	const row = document.createElement("div");
+
+	for (let i = 1; i <= n; i++) {
+		const item = document.createElement("div");
+		item.classList.add("item");
+
+		row.appendChild(item);
+	}
+
+	row.classList.add("row");
+	gridContainer.appendChild(row);
+}
