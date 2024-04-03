4a10f2ae5718f098ef837ef390dacc9c51845610
Joshua Mamawag <162310653+jjjmdev@users.noreply.github.com>
2024-04-03 22:04:06 +0800

    Randomize background color

--- scripts.js
+++ scripts.js
@@ -10,11 +10,23 @@ for (let j = 1; j <= n; j++) {
 		item.classList.add("item");
 
 		item.addEventListener("mouseenter", () => {
-			item.style.backgroundColor = "black";
+			if (!item.style.backgroundColor) {
+				item.style.backgroundColor = randomColor();
+			}
 		});
 		row.appendChild(item);
 	}
 
 	row.classList.add("row");
 	gridContainer.appendChild(row);
+}
+
+function randomNumber(number) {
+	return Math.floor((Math.random() * number * 10) / 10);
+}
+
+function randomColor() {
+	return `rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(
+		255
+	)})`;
 }
